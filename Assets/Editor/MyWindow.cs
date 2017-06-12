771dca7153283329e192fd0ec5a4664177180e75
laishencan <laishencan@laishencandeMac-mini.local>
2017-06-12 12:08:35 +0800

    gui editor

--- Assets/Editor/MyWindow.cs
+++ Assets/Editor/MyWindow.cs
@@ -3,16 +3,12 @@ using UnityEngine;
 
 public class MyWindow: EditorWindow
 {
-    string myString = "hello";
-    bool groupEnabled;
-    bool myBool;
-    float myFloat;
-    Object myObj;
-
-
+    string url;
+    string key;
+    
     string path;  
+    string packagePath;
     Rect rect;  
-
 
     [MenuItem("Tool/ShowWindow")]
     public static void ShowWindow()
@@ -27,22 +23,36 @@ public class MyWindow: EditorWindow
     /// </summary>
     void OnGUI()
     {
-        GUILayout.Label("BaseSetting", EditorStyles.boldLabel); 
-        path = filePathReceiver("路径", 500, path);
+        EditorGUI.LabelField(new Rect(0, 0, 100, 20), "json path");
+        path = filePathReceiver(new Rect(100, 0, 500, 20), path);
+
+        if(GUI.Button(new Rect(0, 30, 100, 50), "Get"))
+        {
+            url = Tool.GetUrl(path); 
+            key = Tool.GetKey(path); 
+        }
+
+        EditorGUI.LabelField(new Rect(0, 90, 100, 20), "url");
+        EditorGUI.SelectableLabel(new Rect(100, 90, 500, 20), url);
+
+        EditorGUI.LabelField(new Rect(0, 120, 100, 20), "key");
+        EditorGUI.SelectableLabel(new Rect(100, 120, 500, 20), key);
+
+        EditorGUI.LabelField(new Rect(0, 180, 100, 20), "package path");
+        packagePath = filePathReceiver(new Rect(100, 180, 500, 20), packagePath);
+
+        if(GUI.Button(new Rect(0, 220, 100, 50), "Decrypt"))
+        {
+            Tool.DecryptUnityPackage(packagePath, key);
+        }
     }
-    
 
-    string filePathReceiver(string title, int labelWidth, string content)
+    string filePathReceiver(Rect rect, string content)
     {
-        EditorGUILayout.BeginHorizontal();
-
-            EditorGUI.LabelField(new Rect(3, 3, position.width, 20), title);  
-            //获得一个长width的框  
-            rect = EditorGUILayout.GetControlRect(GUILayout.Width(labelWidth));  
-            //将上面的框作为文本输入框  
-            content = EditorGUI.TextField(rect, path);  
-
-        EditorGUILayout.EndHorizontal();
+        // //获得一个长width的框  
+        // rect = EditorGUILayout.GetControlRect(GUILayout.Width(labelWidth));  
+        // //将上面的框作为文本输入框  
+        content = EditorGUI.TextField(rect, content);  
 
         //如果鼠标正在拖拽中或拖拽结束时，并且鼠标所在位置在文本输入框内  
         if ((Event.current.type == EventType.DragUpdated  
--- Assets/Editor/Tool.cs
+++ Assets/Editor/Tool.cs
@@ -23,23 +23,37 @@ class Tool
 	//用 其他下载工具下载 url, 得到 未解析的插件文件
 	//用 反射 UnityEditor.AssetStoreUtils.DecryptFile 解析 插件文件
 
-	[MenuItem("Tool/DecryptUnityPackage")]
-	static void DecryptUnityPackage()
+	public static void DecryptUnityPackage(string inputFile, string key)
 	{
-		string inputFile = Application.dataPath + "/AssetStore/434fe5c2-6f1b-42cd-b690-b55699522122";
-		string jsonFilePath = "/Users/Projects/AssetStoreDownload/Assets/AssetStore/Survival Shooter tutorial-content__40756.tmp.json";
-
-		string json = System.IO.File.ReadAllText(jsonFilePath);
-		Download downloadInfo = JsonUtility.FromJson<Download>(json);
-
-		string url = downloadInfo.download.url;
-		string key = downloadInfo.download.key;
-
-return;
+		// string inputFile = Application.dataPath + "/AssetStore/434fe5c2-6f1b-42cd-b690-b55699522122";
         System.Reflection.Assembly unityEditor = typeof(Editor).Assembly;  
   
         System.Type assetStoreUtils = unityEditor.GetType("UnityEditor.AssetStoreUtils");  
   
         assetStoreUtils.Invoke("DecryptFile", inputFile, inputFile + ".unitypackage", key);  
+	}
+
+	public static string GetUrl(string jsonFilePath)
+	{
+		string json = System.IO.File.ReadAllText(jsonFilePath);
+		Download downloadInfo = JsonUtility.FromJson<Download>(json);
+
+		string url = downloadInfo.download.url;
+
+		return url;
+	}
+
+	public static string GetKey(string jsonFilePath)
+	{
+		string json = System.IO.File.ReadAllText(jsonFilePath);
+		Download downloadInfo = JsonUtility.FromJson<Download>(json);
+
+		string key = downloadInfo.download.key;
+
+		return key;
+	}
+
+	public static void DownloadUrl(string url)
+	{
 	}
 }
